b5cc2bb73c5297a00f22ce78ab22ba663068bf59
lianmang-cs <lianmang238@gmail.com>
2026-02-14 17:30:51 -0600

    Animate player and add sound (bg).
    Added an animation for the player and added a background audio. For the player, I added animations for: idle, walk, fish, hook. For the audio, I added a source to the water object.

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerBehaviour : MonoBehaviour
 {
+    public float speed = 0.004f; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +12,17 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float offset = 0.0f; 
+        //Left move
+        if (Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed) {
+            offset = -speed;
+        }
+        //Right move
+        if (Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.isPressed) {
+            offset = speed; 
+        } 
+        Vector3 newPos = transform.position;
+        newPos.x = newPos.x + offset; 
+        transform.position = newPos; 
     }
 }
