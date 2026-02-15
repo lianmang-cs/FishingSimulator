16f6afa29ddcb0f916d33043edf76515d207f2b9
lianmang-cs <lianmang238@gmail.com>
2026-02-14 19:43:30 -0600

    Flip the player left and right.
    Made it so that when I move the player left or right, it can face the left or right direction respectively.

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -2,13 +2,12 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerBehaviour : MonoBehaviour
 {
-    public float speed = 0.004f; 
+    public float speed;  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -16,13 +15,22 @@ public class PlayerBehaviour : MonoBehaviour
         //Left move
         if (Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed) {
             offset = -speed;
+            //flip left
+            Vector3 scale = transform.localScale; 
+            scale.x = -Mathf.Abs(scale.x);
+            transform.localScale = scale; 
         }
         //Right move
         if (Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.isPressed) {
             offset = speed; 
+            //flip right
+            Vector3 scale = transform.localScale; 
+            scale.x = Mathf.Abs(scale.x);
+            transform.localScale = scale; 
         } 
         Vector3 newPos = transform.position;
         newPos.x = newPos.x + offset; 
         transform.position = newPos; 
     }
+    
 }
