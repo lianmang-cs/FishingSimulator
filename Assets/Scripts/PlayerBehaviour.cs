23a3fd0730919ecb25e47044f1a892efd9440193
lianmang-cs <lianmang238@gmail.com>
2026-03-21 09:46:12 -0500

    Made transitions to the animation.
    Added a transition from the player idle animation to the walk animation when the player moves. When the player doesn't move, I added a transition back to the idle animation. Then, I created float speed parameter for the transition and adjusted it within the player behaviour script to seamlessly transition between the idle and walk animations for the player.

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerBehaviour : MonoBehaviour
 {
-    public float speed;  
+    public float movSpeed;
+    public float animSpeed;    
+    public Animator animator; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,23 +16,33 @@ public class PlayerBehaviour : MonoBehaviour
         float offset = 0.0f; 
         //Left move
         if (Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed) {
-            offset = -speed;
+            offset = -movSpeed;
             //flip left
             Vector3 scale = transform.localScale; 
             scale.x = -Mathf.Abs(scale.x);
             transform.localScale = scale; 
+            //animation speed
+            animSpeed = 1; 
         }
         //Right move
-        if (Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.isPressed) {
-            offset = speed; 
+        else if (Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.isPressed) {
+            offset = movSpeed; 
             //flip right
             Vector3 scale = transform.localScale; 
             scale.x = Mathf.Abs(scale.x);
             transform.localScale = scale; 
+            //animation speed
+            animSpeed = 1; 
         } 
+        else {
+            animSpeed = 0; 
+        }
         Vector3 newPos = transform.position;
         newPos.x = newPos.x + offset; 
         transform.position = newPos; 
+        //update animation speed
+        animator.SetFloat("Speed", Mathf.Abs(animSpeed)); 
+
     }
     
 }
