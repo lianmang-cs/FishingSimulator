66eda144830783671f364dedfc801f689d5862c4
lianmang-cs <lianmang238@gmail.com>
2026-03-22 17:32:35 -0500

    Added an attack animation for the boar.
    I added an attack animation for the boar, but will not use it unit I can figure out how to get the mechanics for the fox.

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public float distance;
+    public float speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+}
