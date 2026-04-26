585dca9237fcc2dfc4f5d010355522708ba6c3de
lianmang-cs <lianmang238@gmail.com>
2026-04-26 13:13:53 -0500

    Set the max and min distance the player can travel.
    The player cannot go pass the pier and outside of the game window. This was done by adjusting the min and max travel distance in the player behavior script and then tested values in the inspector.

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -3,16 +3,14 @@ using UnityEngine.InputSystem;
 public class PlayerBehaviour : MonoBehaviour
 {
     public float movSpeed;
-    public float animSpeed;    
+    public float animSpeed; 
+    public float min;
+    public float max;   
     public Animator animator; 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
     // Update is called once per frame
     void Update()
     {
+        
         float offset = 0.0f; 
         //Left move
         if (Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed) {
@@ -21,7 +19,7 @@ public class PlayerBehaviour : MonoBehaviour
             Vector3 scale = transform.localScale; 
             scale.x = -Mathf.Abs(scale.x);
             transform.localScale = scale; 
-            //animation speed
+            //set animation to walk
             animSpeed = 1; 
         }
         //Right move
@@ -31,17 +29,28 @@ public class PlayerBehaviour : MonoBehaviour
             Vector3 scale = transform.localScale; 
             scale.x = Mathf.Abs(scale.x);
             transform.localScale = scale; 
-            //animation speed
+            //set animation to walk
             animSpeed = 1; 
         } 
         else {
+            //set animation to idle
             animSpeed = 0; 
         }
-        Vector3 newPos = transform.position;
+         Vector3 newPos = transform.position;
         newPos.x = newPos.x + offset; 
+        // Prevent movement too far right
+        if (newPos.x > max)
+        {
+        newPos.x = max;
+        }
+        // Prevent movement too far left
+        if (newPos.x < min)
+        {
+            newPos.x = min;
+        }
         transform.position = newPos; 
-        //update animation speed
-        animator.SetFloat("Speed", Mathf.Abs(animSpeed)); 
+        //pass the animSpeed trigger walk/move when key is pressed or not
+        animator.SetFloat("Speed", animSpeed); 
 
     }
     
