e910af118ba64fa980aa03e78680870c33869e59
lianmang-cs <lianmang238@gmail.com>
2026-04-26 15:29:03 -0500

    Made the fishes move at random positions.
    I attached the fish behavior script to the base prefab and wrote the script to make the fish move randomly within the bounds of the water.

--- Assets/Scripts/FishBehaviour.cs
+++ Assets/Scripts/FishBehaviour.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class FishBehaviour : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    //set boundries of the water for the fish
+    public float leftBound;
+    public float rightBound; 
+    public float topBound; 
+    public float bottomBound; 
+    //speed of the fish
+    public float fishSpeed; 
+    //time until moving to new random position
+    public float waitTime;
+
+    private Vector3 randomPos;
+    private float timer;  
 
     // Update is called once per frame
     void Update()
     {
-        
+        //count down the timer
+        timer -= Time.deltaTime; 
+        //time to move randomly 
+        if(timer <= 0) {
+        //randomize the position of the fish in the boundries
+        float randomLR = Random.Range(leftBound, rightBound);
+        float randomTB = Random.Range(topBound, bottomBound); 
+        randomPos = new Vector3(randomLR, randomTB, transform.position.z); 
+        //reset timer
+        timer = waitTime; 
+        }
+
+        //move to the random position
+        transform.position = Vector3.MoveTowards(transform.position, randomPos, fishSpeed);     
     }
+    
 }
